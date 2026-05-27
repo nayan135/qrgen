945f820d82f1cdee0f87db099bbf52204f96000d
NAYAN ACHARYA <100992818+nayan135@users.noreply.github.com>
2026-05-27 07:59:01 +0545

    committt

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function generateeQr(){
+      let color='#ffffff';
       let qrText = document.getElementById("naayy").value || "https://nayan135.com.np";
       document.getElementsByClassName("url")[0].textContent = qrText;
       document.getElementsByClassName("url")[0].href = qrText;
       document.getElementById("qrcode").innerHTML = "";
+      
       new QRCode(document.getElementById("qrcode"), {
       text: qrText,
        width:256,
         height:256,
-         colorDark: '#000000',
-          colorLight:'#ffffff', 
+       //  colorDark: '#000000',
+          colorLight: color, 
           correctLevel: QRCode.CorrectLevel.H  
       });
       
@@ -16,11 +18,11 @@ function generateeQr(){
 
 function downloadQr(){
       let qrCanvas = document.querySelector("#qrcode canvas");
-      if(canvas){
+      if(qrCanvas){
             let image=qrCanvas.toDataURL('image/png');
             let link=document.createElement('a');
             link.href=image;
-            link.download=qrText+'.png';
+            link.download='qrrr.png';
             link.click();
       }
 }
